d393ea404d094eb61ec68d9979764e4177bf3fbf
kshitijrat <kshitijratnawat@gmail.com>
2025-04-15 10:53:12 +0530

    first commit

---
+++ src/main/java/com/springmvcformapp/spring_mcv_form_app/SpringMcvFormAppApplication.java
@@ -0,0 +1,13 @@
+package com.springmvcformapp.spring_mcv_form_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcvFormAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMcvFormAppApplication.class, args);
+	}
+
+}
