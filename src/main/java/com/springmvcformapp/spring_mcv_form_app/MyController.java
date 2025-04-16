8cfe17374f9f694fce2f1f6a5ee0c8509cff42d3
kshitijrat <kshitijratnawat@gmail.com>
2025-04-16 13:46:37 +0530

    update

--- src/main/java/com/springmvcformapp/spring_mcv_form_app/MyController.java
+++ src/main/java/com/springmvcformapp/spring_mcv_form_app/MyController.java
@@ -2,8 +2,9 @@ package com.springmvcformapp.spring_mcv_form_app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyController {
     
     @GetMapping("/")
